b442096a44f6c2ab30f7acc7f6f95deddf3fea37
Taha Tosun <87184578+tahatosunn@users.noreply.github.com>
2023-07-12 18:28:26 +0300

    swift package init

--- Package.swift
+++ Package.swift
@@ -1,31 +1,23 @@
-// swift-tools-version:5.3
-// Bu satırda, Swift sürümünü belirtmelisiniz.
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "KokteylLog",
-    platforms: [
-        .iOS(.v9) // veya projenin gereksinimlerine göre değiştirin
-    ],
+    name: "KOKTEYL-LOG-SDK",
     products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "KokteylLog",
-            targets: ["KokteylLog"]
-        )
+            name: "KOKTEYL-LOG-SDK",
+            targets: ["KOKTEYL-LOG-SDK"]),
     ],
-    dependencies: [],
     targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "KokteylLog",
-            dependencies: [],
-            path: "KKLog",
-            resources: [
-                .process("KKLog.xcframework")
-            ],
-            cSettings: [
-                .headerSearchPath("KKLog/include"),
-            ]
-        )
+            name: "KOKTEYL-LOG-SDK"),
+        .testTarget(
+            name: "KOKTEYL-LOG-SDKTests",
+            dependencies: ["KOKTEYL-LOG-SDK"]),
     ]
 )
---
+++ Tests/KOKTEYL-LOG-SDKTests/KOKTEYL_LOG_SDKTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import KOKTEYL_LOG_SDK
+
+final class KOKTEYL_LOG_SDKTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documenation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
