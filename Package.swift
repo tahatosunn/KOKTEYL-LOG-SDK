0026722b8821d77a2190b244390f661b4a3e7b41
Taha Tosun <87184578+tahatosunn@users.noreply.github.com>
2023-07-12 17:03:24 +0300

    SPM support added.

---
+++ Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version:5.3
+// Bu satırda, Swift sürümünü belirtmelisiniz.
+
+import PackageDescription
+
+let package = Package(
+    name: "KokteylLog",
+    platforms: [
+        .iOS(.v9) // veya projenin gereksinimlerine göre değiştirin
+    ],
+    products: [
+        .library(
+            name: "KokteylLog",
+            targets: ["KokteylLog"]
+        )
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "KokteylLog",
+            dependencies: [],
+            path: "KKLog",
+            resources: [
+                .process("KKLog.xcframework")
+            ],
+            cSettings: [
+                .headerSearchPath("KKLog/include"),
+                .unsafeFlags(["-ObjC"])
+            ]
+        )
+    ]
+)
