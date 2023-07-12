d6e4fbaa2972bcb9d45c457ef65511ecf49d41f4
Taha Tosun <87184578+tahatosunn@users.noreply.github.com>
2023-07-12 18:23:11 +0300

    unsafe flag removed

--- Package.swift
+++ Package.swift
@@ -25,7 +25,6 @@ let package = Package(
             ],
             cSettings: [
                 .headerSearchPath("KKLog/include"),
-                .unsafeFlags(["-ObjC"])
             ]
         )
     ]
