2abbd1365938d347093e4cd1572f79e8fe5de95b
Taha Tosun <87184578+tahatosunn@users.noreply.github.com>
2023-07-13 10:40:57 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,13 +12,16 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "KOKTEYL-LOG-SDK",
-            targets: ["KKLog"]),
+            targets: ["KOKTEYL-LOG-SDK"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "KOKTEYL-LOG-SDK"),
+            name: "KOKTEYL-LOG-SDK",
+            dependencies: [
+                .target(name: "KKLog")
+            ]),
         .testTarget(
             name: "KOKTEYL-LOG-SDKTests",
             dependencies: ["KOKTEYL-LOG-SDK"]),
